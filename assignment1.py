eb10d6af9e54bd2140f9caa8ec578d60d3244ac2
Kameron Dam <kdam3@myseneca.ca>
2024-11-16 01:01:48 -0500

    12

--- assignment1.py
+++ assignment1.py
@@ -34,7 +34,7 @@ def leap_year(year: int) -> bool:
     return (year % 4 == 0 and year % 100 !=0) or (year % 400 == 0)
     
 
-def mon_max(month:int, year:int) -> int:
+def mon_max(month: int, year: int) -> int:
     "returns the maximum day for a given month. Includes leap year check"
     month_days = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11:30, 12: 31}
     if month == 2 and leap_year(year):
