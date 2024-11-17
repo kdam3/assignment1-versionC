451211064d22a742d6dbd4951644045e560c6968
Kameron Dam <kdam3@myseneca.ca>
2024-11-16 22:53:55 -0500

    16

--- assignment1.py
+++ assignment1.py
@@ -32,7 +32,6 @@ def day_of_week(date: str) -> str:
 def leap_year(year: int) -> bool:
     "return true if the year is a leap year"
     return (year % 4 == 0 and year % 100 !=0) or (year % 400 == 0)
-    
 
 def mon_max(month: int, year: int) -> int:
     "returns the maximum day for a given month. Includes leap year check"
